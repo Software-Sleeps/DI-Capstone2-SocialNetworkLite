47edbfab3d3f9fd10268dd76d7d23b071f0035f3
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-06-22 13:29:33 -0400

    organizing and deleting files

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Navigation from '../Navigation/Navigation';
+import AccountOverview from './AccountOverview/AccountOverview';
 
 class Account extends Component {
     render() {
         return (
             <div>
                 <Navigation/>
-                <h1>ViewAccounts!</h1>
+                
+                <div>
+                    <AccountOverview/>
+                </div>
             </div>
         );
     }
---
+++ src/components/Account/AccountOverview/AccountOverview.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./AccountOverview.css";
+class AccountOverview extends Component {
+  render() {
+    return (
+      <div className="p-input">
+        <div>
+          <div>
+            
+          </div>
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AccountOverview;
