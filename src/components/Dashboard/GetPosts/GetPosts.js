13037bc21f2b39626cbf18205fa8b40f0dfcb79e
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-06-24 09:26:45 -0400

    error popped up

--- src/components/Dashboard/GetPosts/GetPosts.js
+++ src/components/Dashboard/GetPosts/GetPosts.js
@@ -12,7 +12,7 @@ class GetPosts extends Component {
       //Temporary
       URL: "https://socialnetworklite.herokuapp.com",
       username: JSON.parse(sessionStorage.getItem('username')),
-      likeID: null,
+      likedPosts: [],
       liked: false
     };
 
@@ -37,7 +37,8 @@ class GetPosts extends Component {
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    this.setState({liked: true})
+    this.setState({likedPosts: data.likes, liked: true})
+
   }).catch(error => console.log(error))
   }
 
@@ -57,6 +58,13 @@ class GetPosts extends Component {
   }
 
   componentDidMount() {
+
+    // this.state.posts.forEach((element) =>{
+    //     element.likes.forEach(nestedElement => {
+    //      this.state.likedPosts.push(nestedElement)
+    //     })
+    //   })
+    // console.log('these are the liked posts', this.state.likedPosts)
     const userToken = JSON.parse(sessionStorage.getItem("token"));
 
     fetch(`${this.state.URL}/posts?limit=5`, {
@@ -70,7 +78,7 @@ class GetPosts extends Component {
       .then((data) => {
         console.log('posts', data)
         this.setState({
-          posts: data.posts,
+          posts: data,
         });
 
         console.log('checking username',JSON.parse(sessionStorage.getItem('username')));
@@ -91,12 +99,12 @@ class GetPosts extends Component {
     // })
 
     //a boolean to check if user has liked or not
-    const userLikes = this.state.posts.map((element) =>{
+    // const userLikes = this.state.posts.map((element) =>{
 
-     let un = element.likes.find(element => element.username === this.state.username)
+    //  let un = element.likes.find(element => element.username === this.state.username)
 
-     if (un){ return true} else {return false}
-    })
+    //  if (un){ return true} else {return false}
+    // })
 
     //all cards
     const allPosts = this.state.posts.map((element, index) => {
@@ -119,7 +127,7 @@ class GetPosts extends Component {
             </Toast.Body>
             
           
-        {!userLikes ?(
+        {this.state.liked ?(
           <Button style={{backgroundColor: "#9A6A5C", border:"none"}}type="submit" value="submit" onClick={() => this.addLike(element.id)}>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-suit-heart" viewBox="0 0 16 16">
           <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
